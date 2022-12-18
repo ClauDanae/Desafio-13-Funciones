686e453d1dbe950675e52a6f7e0ab7e2371e72ef
Clau Leiva <cleiva.hz@gmail.com>
2022-12-17 21:06:14 -0300

    Terminado el requerimiento 3

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,8 @@
-<script>
-function pintar(){
-ele = document.getElementById("ele1")
-ele.style.backgroundColor = 'yellow'
+function pintar(ele, color) {
+    ele.style.backgroundColor = color
 }
 
 ele = document.getElementById("ele1");
-ele.addEventListener("click", pintar);
-</script>
+ele.addEventListener("click", function () {
+    pintar(ele, "yellow")
+});
